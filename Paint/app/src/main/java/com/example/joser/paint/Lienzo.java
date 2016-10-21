c865763e31f3063626803c821e9bd6f70b68243c
Joserg93 <joserg11@hotmail.com>
2016-10-20 21:39:34 -0600

    1.0

---
+++ Paint/app/src/main/java/com/example/joser/paint/Lienzo.java
@@ -0,0 +1,18 @@
+package com.example.joser.paint;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by joser on 20/10/2016.
+ */
+public class Lienzo extends View {
+    public Lienzo(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public void setupDrawing(){
+
+    }
+}
